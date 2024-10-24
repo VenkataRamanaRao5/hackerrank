e69162fa4682b0fadda507ae1f914b9882ef12fe
VenkataRamanaRao5 <venkatraocv2005@gmail.com>
2024-10-24 21:10:08 +0530

    Revert "Added svg badge icon files"
    This reverts commit afe72411e08d652b6e67ff2f05d4164adbca1cdd.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const https = require("https")
 const express = require("express")
 const cheerio = require('cheerio')
-const fs = require('fs')
 
 const app = express()
 
@@ -25,11 +24,9 @@ app.get('/:username', (req, res) => {
             const $ = cheerio.load(data)
             console.log($('.profile-title-wrapper :first').text())
             const badges = $('.hacker-badges-v2 .hacker-badge');
-            const numBadges = badges.length
-            const width = 500, height = (numBadges < 5) ? 160 : 270
 
             // Initialize SVG header
-            let svgContent = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}px" height="${height}px" style="border:solid black 1px; border-radius:2px; box-sizing: border-box;" href="https://hackerrank.com/profile/${username}">`;
+            let svgContent = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="270px" style="border:solid black 1px; border-radius:2px;" href="https://hackerrank.com/profile/${username}">`;
             svgContent += `<text x="10" y="23" font-size="24" font-weight="bold" font-family="Satoshi, Open Sans, sans-serif">${$('.profile-title-wrapper :first').text()}</text>`
             svgContent += `<text x="10" y="40" font-size="14" font-family="Satoshi, Open Sans, sans-serif">@${username}</text>`
             svgContent += `<g transform="translate(410,5)">${$('.hacker-badges .section-card-header :first').html()}`
@@ -37,13 +34,8 @@ app.get('/:username', (req, res) => {
 
             // Loop through the badges and extract SVGs
             badges.each((index, element) => {
-                const name = $(element)._findBySelector('image.badge-icon').attr().href.split('/').reverse()[0]
-                console.log(name)
-                const data = `<svg x="25.5" y="22" height="27" width="40">${fs.readFileSync('assets/' + name, 'utf-8')}</svg>`
-                $(element)._findBySelector('image.badge-icon').replaceWith(data)
                 const badgeSvg = $(element).find('svg').html() // Extract the inner SVG content
                 svgContent += `<svg width="91" height="100" x="${(index % 5) * 95 + 10}" y="${Math.floor(index / 5) * 110 + 55}" style="text-anchor:middle">${badgeSvg}</svg>`; // Position badges vertically
-                
             });
 
             svgContent += '</svg>';
@@ -61,7 +53,6 @@ app.get('/:username', (req, res) => {
 
     proxyRequest.end()
 })
-
 
 const PORT = 3000
 app.listen(PORT, () => {
